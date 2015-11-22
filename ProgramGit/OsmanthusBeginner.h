691e610a1e0978e01d4b1a2e6df26a86f458e3b7
Eqistyre <stillalive@aliyun.com>
2015-11-22 21:16:21 +0800

    OsmanthusBeginner

---
+++ ProgramGit/OsmanthusBeginner.h
@@ -0,0 +1,20 @@
+#ifndef _OSMANTHUS_BEGINNER_H_
+#define _OSMANTHUS_BEGINNER_H_
+
+#include "cocos2d.h"
+
+class OsmanthusBeginner : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(OsmanthusBeginner);
+};
+
+#endif // __HELLOWORLD_SCENE_H__

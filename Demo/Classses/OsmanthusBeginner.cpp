3e54d9e57a2f4afea13d2772bd0bdbaca45838f3
Eqistyre <stillalive@aliyun.com>
2015-11-28 18:35:43 +0800

    Button change
    Just when you ended touch,the program will be ended

--- Demo/Classses/OsmanthusBeginner.cpp
+++ Demo/Classses/OsmanthusBeginner.cpp
@@ -35,7 +35,10 @@ bool OsmanthusBeginner::init()
     auto rootNode = CSLoader::createNode("OsmanthusBeginner.csb");
     addChild(rootNode);
     
-    cocos2d::ui::Button*  BeginnerStartBtn = (cocos2d::ui::Button*)rootNode->getChildByName("BeginnerStartBtn");
+    cocos2d::ui::Button* BeginnerStartBtn = (cocos2d::ui::Button*)rootNode->getChildByName("BeginnerStartBtn");
+    cocos2d::ui::Button* BeginnerContinueBtn = (cocos2d::ui::Button*)rootNode->getChildByName("BeginnerContinueBtn");
+    cocos2d::ui::Button* BeginnerMenuBtn = (cocos2d::ui::Button*)rootNode->getChildByName("BeginnerMenuBtn");
+    cocos2d::ui::Button* BeginnerExitBtn = (cocos2d::ui::Button*)rootNode->getChildByName("BeginnerExitBtn");
     
     BeginnerStartBtn->addTouchEventListener([](Ref* pSender,cocos2d::ui::Widget::TouchEventType type) {
         switch(type) {
@@ -58,6 +61,70 @@ bool OsmanthusBeginner::init()
             default:
                 break;
         }
+    });
+    
+    BeginnerContinueBtn->addTouchEventListener([](Ref* pSender,cocos2d::ui::Widget::TouchEventType type) {
+        switch (type) {
+            case cocos2d::ui::Widget::TouchEventType::BEGAN:
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::MOVED:
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::ENDED:
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::CANCELED:
+                break;
+                
+            default:
+                break;
+        }
+        
+    });
+    
+    BeginnerMenuBtn->addTouchEventListener([](Ref* pSender,cocos2d::ui::Widget::TouchEventType type) {
+        switch(type) {
+            case cocos2d::ui::Widget::TouchEventType::BEGAN:
+                CCLOG("BEGAN");
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::MOVED:
+                CCLOG("MOVED");
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::ENDED:
+                CCLOG("ENDED");
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::CANCELED:
+                CCLOG("CANCELED");
+                break;
+                
+            default:
+                break;
+        }
+    });
+
+    BeginnerExitBtn->addTouchEventListener([](Ref* pSender,cocos2d::ui::Widget::TouchEventType type) {
+        switch (type) {
+            case cocos2d::ui::Widget::TouchEventType::BEGAN:
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::MOVED:
+                break;
+            //just when you ended touch,the program br exited
+            case cocos2d::ui::Widget::TouchEventType::ENDED:
+                exit(0);
+                break;
+                
+            case cocos2d::ui::Widget::TouchEventType::CANCELED:
+                break;
+                
+            default:
+                break;
+        }
+        
     });
     return true;
 }
